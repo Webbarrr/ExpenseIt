c3df84707b8f732bc2d9ba848e7e0f1102e3ec4a
James Webber <James.Webber@cansfordlabs.co.uk>
2019-02-08 14:01:03 +0000

    initial build

---
+++ ExpenseIt/ExpenseItHome.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class ExpenseItHome
+    ' view expense report
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        NavigationService.Navigate(New ExpenseReportPage(peopleListBox.SelectedItem))
+    End Sub
+End Class
---
+++ ExpenseIt/ExpenseReportPage.xaml.vb
@@ -0,0 +1,17 @@
+﻿Class ExpenseReportPage
+    Inherits Page
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Public Sub New(ByVal data As Object)
+        Me.New()
+        DataContext = data
+    End Sub
+End Class
